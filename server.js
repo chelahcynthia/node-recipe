3bbbb62e703ec2c67d92376bc5cd9e5d7de26512
chelahcynthia <cchelangat989@gmail.com>
2023-09-18 18:28:09 +0300

    Adds about content

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs')
 
 const hostname = '0.0.0.0';
 const port = 3500;
 
 const homePage = fs.readFileSync('main.html')
-const aboutPage = fs.readFileSync('about.html');
+const aboutPage = fs.readFileSync('about.html')
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.write(homePage)
+    if (req.url == '/'){
+        res.write(homePage)
+    } else if (req.url == '/about'){
+        res.write(aboutPage)
+    }else if (req.url.match(/images/g)){
+        try{
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "image/jpeg");
+            imgLoc = req.url.replace('/', '../')
+            image = fs.readFileSync(imgLoc)
+            res.end(image);
+        } catch {
+            res.statusCode = 404;
+            res.write("404");
+            console.log(req.url)
+        }
+    } else {
+        res.write('404')
+    }
     res.end();
 });
 
