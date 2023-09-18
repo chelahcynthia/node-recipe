f1c4708611e52b837167a6e89eff7b21012fb4e0
chelahcynthia <cchelangat989@gmail.com>
2023-09-18 18:07:14 +0300

    Creates about file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hostname = '0.0.0.0';
 const port = 3500;
 
 const homePage = fs.readFileSync('main.html')
+const aboutPage = fs.readFileSync('about.html');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
