ae943efbafdee45cf2c55433a695966f13a2c2ca
chelahcynthia <cchelangat989@gmail.com>
2023-09-19 09:55:16 +0300

    Creates menu file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 
 const hostname = '0.0.0.0';
 const port = 3500;
@@ -10,25 +10,7 @@ const aboutPage = fs.readFileSync('about.html')
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    if (req.url == '/'){
-        res.write(homePage)
-    } else if (req.url == '/about'){
-        res.write(aboutPage)
-    }else if (req.url.match(/images/g)){
-        try{
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "image/jpeg");
-            imgLoc = req.url.replace('/', '../')
-            image = fs.readFileSync(imgLoc)
-            res.end(image);
-        } catch {
-            res.statusCode = 404;
-            res.write("404");
-            console.log(req.url)
-        }
-    } else {
-        res.write('404')
-    }
+    res.write(homePage)
     res.end();
 });
 
